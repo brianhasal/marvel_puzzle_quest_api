00a9f748aa33513f66b7476b493c4717771d465c
Brian Hasal <brian.hasal@gmail.com>
2022-06-09 19:20:50 -0700

    Added MyHero model, which yielded my_heros

---
+++ db/migrate/20220610021651_create_my_heros.rb
@@ -0,0 +1,13 @@
+class CreateMyHeros < ActiveRecord::Migration[7.0]
+  def change
+    create_table :my_heros do |t|
+      t.integer :user_id
+      t.integer :hero_id
+      t.integer :first_power_count
+      t.integer :second_power_count
+      t.integer :third_power_count
+
+      t.timestamps
+    end
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,32 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_06_10_013020) do
+ActiveRecord::Schema[7.0].define(version: 2022_06_10_021651) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "heros", force: :cascade do |t|
+    t.string "name"
+    t.integer "stars"
+    t.integer "character_id"
+    t.integer "cover_art_id"
+    t.string "game_description"
+    t.string "first_power"
+    t.string "second_power"
+    t.string "third_power"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "my_heros", force: :cascade do |t|
+    t.integer "user_id"
+    t.integer "hero_id"
+    t.integer "first_power_count"
+    t.integer "second_power_count"
+    t.integer "third_power_count"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "username"
